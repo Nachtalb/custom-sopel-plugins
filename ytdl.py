7bc96e5c9e19edca79e3afe8637e33817c65471f
xnaas <me@xnaas.info>
2021-09-20 13:55:29 -0500

    add tiktok exception for ytdl

--- ytdl.py
+++ ytdl.py
@@ -61,6 +61,11 @@ def ytdl(bot, trigger):
             ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
             return
+        if re.search(r"vm\.tiktok\.com\/", url):
+            bot.say(formatting.italic("Downloading..."))
+            ytdl.download([url])
+            bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
+            return
         else:
             bot.reply(
                 "This video has no duration (livestream?) and cannot be downloaded.")
