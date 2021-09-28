35dc311ff4a8be361f8ddb8466f91bcf022a6ab7
xnaas <me@xnaas.info>
2021-09-28 15:54:50 -0500

    add youtube link logging, expand ytdl command

--- ytdl.py
+++ ytdl.py
@@ -1,9 +1,58 @@
-from sopel import plugin, tools, formatting
-import os
+from sopel import plugin
+from sopel.formatting import bold, italic
+from sopel.tools import SopelIdentifierMemory
 import re
 import yt_dlp as youtube_dl
 
 
+# YouTube Link Logger
+def setup(bot):
+    if "youtube_ids" not in bot.memory:
+        bot.memory["youtube_ids"] = SopelIdentifierMemory()
+
+# YouTube Link Logger
+
+
+def shutdown(bot):
+    try:
+        del bot.memory["youtube_ids"]
+    except KeyError:
+        pass
+
+# YouTube Link Logger
+
+
+@plugin.echo
+@plugin.priority("low")
+@plugin.require_chanmsg
+@plugin.search(
+    r"(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})")
+@plugin.unblockable
+def youtube_link_log(bot, trigger):
+    video_id = trigger.group(6)
+
+    if not video_id:
+        bot.say("xnaas: YouTube link logging error...good luck!")
+        return
+
+    # Logs latest YouTube link per channel
+    bot.memory["youtube_ids"][trigger.sender] = video_id
+
+    # Debugging in Prod
+    # bot.say("Message logged. ID logged: {}".format(bold(video_id)))
+
+
+# Tells us what ID is logged for this channel.
+@plugin.command("ytid")
+def temp_youtube_id(bot, trigger):
+    try:
+        bot.reply("The currently stored ID for this channel is {}.".format(
+            bold(bot.memory["youtube_ids"][trigger.sender])))
+    except KeyError:
+        bot.reply("I have no IDs stored for this channel.")
+
+
+# Download MP4-compatible formats for MP4 container
 ytdl_opts = {
     "format": "bestvideo[height<=?1080]+bestaudio/best",
     "merge_output_format": "mp4",
@@ -20,9 +69,11 @@ def ytdl(bot, trigger):
     url = trigger.group(3)
 
     if not url:
-        bot.reply("I need a link, silly!")
-        return
-    url = tools.Identifier(url)
+        try:
+            url = bot.memory["youtube_ids"][trigger.sender]
+        except KeyError:
+            bot.reply(
+                "You've given me nothing to work with...what the Hell do you want?!")
 
     try:
         with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
@@ -39,7 +90,7 @@ def ytdl(bot, trigger):
                     "This video is longer than 10 minutes and cannot be download, sorry!")
                 return
             else:
-                bot.say(formatting.italic("Downloading..."))
+                bot.say(italic("Downloading..."))
                 ytdl.download([url])
                 bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
                 return
@@ -47,22 +98,22 @@ def ytdl(bot, trigger):
         bot.reply("Please submit a valid link.")
     except KeyError:
         if re.search(r"v\.redd\.it\/", url):
-            bot.say(formatting.italic("Downloading..."))
+            bot.say(italic("Downloading..."))
             ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
             return
         if re.search(r"video\.twimg\.com\/", url):
-            bot.say(formatting.italic("Downloading..."))
+            bot.say(italic("Downloading..."))
             ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
             return
         if re.search(r"cdn\.discordapp\.com\/", url):
-            bot.say(formatting.italic("Downloading..."))
+            bot.say(italic("Downloading..."))
             ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
             return
         if re.search(r"vm\.tiktok\.com\/", url):
-            bot.say(formatting.italic("Downloading..."))
+            bot.say(italic("Downloading..."))
             ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
             return
